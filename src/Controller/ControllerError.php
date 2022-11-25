8bae6f144520cbaea18ab225caba59da2a6f3904
damien MATHIEU <damien.mathieu@etu.umontpellier.fr>
2022-11-25 10:18:24 +0100

    petit début très tchatcheur

---
+++ src/Controller/ControllerError.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Fakex\Controller;
+
+class ControllerError {
+    private static function afficheVue(string $cheminVue, array $parametres = []) : void {
+        extract($parametres); // Crée des variables à partir du tableau $parametres
+        require __DIR__. "/../View/$cheminVue"; // Charge la vue
+    }
+    public static function error() : void {
+        self::afficheVue('Error/generalError.html');
+    }
+}
+?>
--- web/frontController.php
+++ web/frontController.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Fakex\Controller\ControllerError;
 use App\Fakex\Lib\Psr4AutoloaderClass;
 
-require_once __DIR__ . '/../src/lib/Psr4AutoloaderClass.php';
+require_once __DIR__ . '/../src/Lib/Psr4AutoloaderClass.php';
 
 // Instanciate the loader
 $loader = new Psr4AutoloaderClass();
@@ -16,6 +17,25 @@ $loader->register();
 
 
 //On recupere l'action passée sur l'URL
+if (isset($_GET['action'])){
+    if (isset($_GET['controller'])){
+        $action = $_GET['action'];
+        $controller = $_GET['controller'];
+        $controllerClassName = 'App\Covoiturage\Controller\Controller'.ucfirst($controller);
+        if (!class_exists($controllerClassName)){
+            $action = 'error';
+            ControllerError::$action();
+        }
+    }
+}
+else{
+    $action = 'error';
+    ControllerError::$action();
+    $action = 'error';
+    $controller = 'error';
+    $controllerClassName = 'App\Covoiturage\Controller\Controller'.ucfirst($controller);
+    echo 'caca';
+}
 
 $controller = $_GET['controller'] ?? 'standard';
 $controllerClassName = "App\Fakex\Controller\Controller".ucfirst($controller);
