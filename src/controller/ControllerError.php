872d4ba6b61d522d89cc4c40a8c164fb7785de12
Alfonso Jimenez <henry.jimenez-chacon@etu.umontpellier.fr>
2022-11-25 11:22:10 +0100

    conf

--- src/controller/ControllerError.php
+++ src/controller/ControllerError.php
@@ -4,7 +4,7 @@ namespace App\Fakex\Controller;
 class ControllerError {
     private static function afficheVue(string $cheminVue, array $parametres = []) : void {
         extract($parametres); // Crée des variables à partir du tableau $parametres
-        require __DIR__. "/../View/$cheminVue"; // Charge la vue
+        require __DIR__ . "/../view/$cheminVue"; // Charge la vue
     }
     public static function error() : void {
         self::afficheVue('Error/generalError.html');
---
+++ src/model/DataObject/Chaussure.php
@@ -0,0 +1,14 @@
+<?php
+
+class Chaussure
+{
+    private int $idCahussure;
+    private string $nom;
+    private string $taille;
+    private int $prix;
+    private int $idImage;
+
+
+
+
+}
