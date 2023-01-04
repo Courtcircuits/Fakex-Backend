5a3b357dd1e7352cba78012ed00ff0666130a1eb
Tristan-Mihai Radulescu <90451752+Milou666@users.noreply.github.com>
2023-01-04 11:41:50 +0100

    Update Conf.php

--- src/config/Conf.php
+++ src/config/Conf.php
@@ -22,13 +22,13 @@ class Conf {
         'hostname' => 'webinfo.iutmontp.univ-montp2.fr',
         // A l'IUT, vous avez une BDD nommee comme votre login
         // Sur votre machine, vous devrez creer une BDD
-        'database' => 'mathieud',
+        'database' => '',
         // A l'IUT, c'est votre login
         // Sur votre machine, vous avez surement un compte 'root'
-        'login' => 'mathieud',
+        'login' => '',
         // A l'IUT, c'est votre mdp (INE par defaut)
         // Sur votre machine personelle, vous avez creez ce mdp a l'installation
-        'password' => '0vAT_@Hd..UjqWpp'
+        'password' => ''
     );
 
     static public function getLogin() : string {
